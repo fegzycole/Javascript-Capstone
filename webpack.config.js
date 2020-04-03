7e272f7d7b3e1b0dbaa3b488acf4263e53427c90
fegzycole <fergusoniyara@gmail.com>
2020-04-03 22:51:28 +0100

    Add project files and install dependencies

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: ['url-loader', 'file-loader'],
+      },
+    ],
+  },
+};
